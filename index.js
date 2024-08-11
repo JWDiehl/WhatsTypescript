8bf871ec65ceb5a6e58894b1ced65aa93c277e75
Jonathan <jondiehl22@gmail.com>
2024-08-11 15:19:53 -0400

    Working

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Import stylesheets
+require("./style.css");
+const form = document.querySelector('#defineform');
+form.onsubmit = () => {
+    const formData = new FormData(form);
+    console.log(formData);
+    const text = formData.get('defineword');
+    console.log(text);
+    return false; // prevent reload
+};
